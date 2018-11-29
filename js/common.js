a2731933fb3350a83229547c2fcf49c290397122
xyl666 <492113976@qq.com>
2018-11-29 11:49:44 +0800

    修改大图上传失败

--- js/common.js
+++ js/common.js
@@ -39,6 +39,60 @@
       type: mime
     })
   }
+   /*
+        三个参数
+        file：一个是文件(类型是图片格式)，
+        w：一个是文件压缩的后宽度，宽度越小，字节越小
+        objDiv：一个是容器或者回调函数
+        photoCompress()
+         */
+      function photoCompress(file,w){
+        return new Promise(function(resolve,reject){
+          var ready=new FileReader();
+          /*开始读取指定的Blob对象或File对象中的内容. 当读取操作完成时,readyState属性的值会成为DONE,如果设置了onloadend事件处理程序,则调用之.同时,result属性中将包含一个data: URL格式的字符串以表示所读取文件的内容.*/
+          ready.readAsDataURL(file);
+          ready.onload=async function(){
+              var re=this.result;
+              var result = await canvasDataURL(re,w)
+              resolve(result)
+          }
+        })
+      }
+      function canvasDataURL(path, obj){
+        return new Promise(function(resolve,reject){
+          var img = new Image();
+          img.src = path;
+          img.onload = function(){
+              var that = this;
+              // 默认按比例压缩
+              var w = that.width,
+                  h = that.height,
+                  scale = w / h;
+              w = obj.width || w;
+              h = obj.height || (w / scale);
+              var quality = 0.7;  // 默认图片质量为0.7
+              //生成canvas
+              var canvas = document.createElement('canvas');
+              var ctx = canvas.getContext('2d');
+              // 创建属性节点
+              var anw = document.createAttribute("width");
+              anw.nodeValue = w;
+              var anh = document.createAttribute("height");
+              anh.nodeValue = h;
+              canvas.setAttributeNode(anw);
+              canvas.setAttributeNode(anh);
+              ctx.drawImage(that, 0, 0, w, h);
+              // 图像质量
+              if(obj.quality && obj.quality <= 1 && obj.quality > 0){
+                  quality = obj.quality;
+              }
+              // quality值越小，所绘制出的图像越模糊
+              var base64 = canvas.toDataURL('image/jpeg', quality);
+              // 回调函数返回base64的值
+              resolve(base64);
+          }
+        })
+      }
   var request = {
     get: function (url, scope) {
       return new Promise(function (resolve, reject) {
