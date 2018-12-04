fed0cf6cc32cee7cf36064e95965402119a84cdf
xyl666 <492113976@qq.com>
2018-12-04 18:11:48 +0800

    注释完善

--- js/common.js
+++ js/common.js
@@ -2,7 +2,12 @@
   function upLoad(id) {
     document.getElementById(id).click()
   }
-  // 绑定上传input[file]事件
+  /**
+   *
+   *绑定上传input[file]事件
+   * @param {*} idInput 绑定按钮id
+   * @param {*} callback 回调函数返回图片base64
+   */
   function bindImgChange(idInput, callback) {
     document.getElementById(idInput).addEventListener('change', function (e) {
       var file = e.target.files[0];
@@ -17,7 +22,12 @@
       };
     })
   }
-
+  /**
+   *获取url中参数
+   *
+   * @param {*} name 需要获取的参数名
+   * @returns 参数值
+   */
   function GetQueryString(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg); //search,查询？后面的参数，并匹配正则
@@ -39,60 +49,70 @@
       type: mime
     })
   }
-   /*
-        三个参数
-        file：一个是文件(类型是图片格式)，
-        w：一个是文件压缩的后宽度，宽度越小，字节越小
-        objDiv：一个是容器或者回调函数
-        photoCompress()
-         */
-      function photoCompress(file,w){
-        return new Promise(function(resolve,reject){
-          var ready=new FileReader();
-          /*开始读取指定的Blob对象或File对象中的内容. 当读取操作完成时,readyState属性的值会成为DONE,如果设置了onloadend事件处理程序,则调用之.同时,result属性中将包含一个data: URL格式的字符串以表示所读取文件的内容.*/
-          ready.readAsDataURL(file);
-          ready.onload=async function(){
-              var re=this.result;
-              var result = await canvasDataURL(re,w)
-              resolve(result)
-          }
-        })
+  /**
+   *
+   *图片压缩
+   * @param {*} file 文件(类型是图片格式)
+   * @param {*} obj 设置参数{width:转换后的宽度，height:转换后的高度，quality:转后图片质量}
+   * @returns
+   */
+  function photoCompress(file, obj) {
+    return new Promise(function (resolve, reject) {
+      var ready = new FileReader();
+      /*开始读取指定的Blob对象或File对象中的内容. 当读取操作完成时,readyState属性的值会成为DONE,如果设置了onloadend事件处理程序,则调用之.同时,result属性中将包含一个data: URL格式的字符串以表示所读取文件的内容.*/
+      ready.readAsDataURL(file);
+      ready.onload = async function () {
+        var re = this.result;
+        var result = await canvasDataURL(re, obj)
+        resolve(result)
       }
-      function canvasDataURL(path, obj){
-        return new Promise(function(resolve,reject){
-          var img = new Image();
-          img.src = path;
-          img.onload = function(){
-              var that = this;
-              // 默认按比例压缩
-              var w = that.width,
-                  h = that.height,
-                  scale = w / h;
-              w = obj.width || w;
-              h = obj.height || (w / scale);
-              var quality = 0.7;  // 默认图片质量为0.7
-              //生成canvas
-              var canvas = document.createElement('canvas');
-              var ctx = canvas.getContext('2d');
-              // 创建属性节点
-              var anw = document.createAttribute("width");
-              anw.nodeValue = w;
-              var anh = document.createAttribute("height");
-              anh.nodeValue = h;
-              canvas.setAttributeNode(anw);
-              canvas.setAttributeNode(anh);
-              ctx.drawImage(that, 0, 0, w, h);
-              // 图像质量
-              if(obj.quality && obj.quality <= 1 && obj.quality > 0){
-                  quality = obj.quality;
-              }
-              // quality值越小，所绘制出的图像越模糊
-              var base64 = canvas.toDataURL('image/jpeg', quality);
-              // 回调函数返回base64的值
-              resolve(base64);
-          }
-        })
+    })
+  }
+  /**
+   *转换图片
+   *
+   * @param {*} path base64编码图片
+   * @param {*} obj 设置参数{width:转换后的宽度，height:转换后的高度，quality:转后图片质量}
+   * @returns
+   */
+  function canvasDataURL(path, obj) {
+    return new Promise(function (resolve, reject) {
+      var img = new Image();
+      img.src = path;
+      img.onload = function () {
+        var that = this;
+        // 默认按比例压缩
+        var w = that.width,
+          h = that.height,
+          scale = w / h;
+        w = obj.width || w;
+        h = obj.height || (w / scale);
+        var quality = 0.7; // 默认图片质量为0.7
+        //生成canvas
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        // 创建属性节点
+        var anw = document.createAttribute("width");
+        anw.nodeValue = w;
+        var anh = document.createAttribute("height");
+        anh.nodeValue = h;
+        canvas.setAttributeNode(anw);
+        canvas.setAttributeNode(anh);
+        ctx.drawImage(that, 0, 0, w, h);
+        // 图像质量
+        if (obj.quality && obj.quality <= 1 && obj.quality > 0) {
+          quality = obj.quality;
+        }
+        // quality值越小，所绘制出的图像越模糊
+        var base64 = canvas.toDataURL('image/jpeg', quality);
+        // 回调函数返回base64的值
+        resolve(base64);
       }
+    })
+  }
+  /**
+   * 请求封装对象
+   */
   var request = {
     get: function (url, scope) {
       return new Promise(function (resolve, reject) {
@@ -124,6 +144,11 @@
     },
   }
 
+  /**
+   *是否登录
+   *根据sessionStorage tel判断是否登录
+   * @returns
+   */
   function isLogin() {
     reg = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/
     return window.sessionStorage.getItem('tel') != null && reg.test(window.sessionStorage.getItem('tel'))
